c32e304d6ed21434f075aeb14def92d8ddf0dfb3
Md Raza Hassan <mdrazahassan@gmail.com>
2025-02-08 20:05:53 +0530

    fixed relaod issue which was happening if user load for first time

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,34 +1,47 @@
 import data from "../../../public/assets/data.json";
 import "./contact.css";
 import { useEffect, useState } from "react";
-import {isUserWithinRadius} from "../location/Location";
+
+import { isUserWithinRadius, getGeolocation } from "../location/Location";
 
 export const Contact = () => {
   const [filteredData, setFilteredData] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [locationFetched, setLocationFetched] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchLocationData = async () => {
       try {
+        const { latitude, longitude } = await getGeolocation();
+
         const filteredLocations = await Promise.all(
           data.map(async (locationData) => {
-            const isWithin = await isUserWithinRadius(locationData.geoLocation);
+            const isWithin = await isUserWithinRadius(
+              locationData.geoLocation,
+              latitude,
+              longitude
+            );
             if (isWithin) {
-              return locationData; 
+              return locationData;
             }
             return null;
           })
         );
-
-        
         setFilteredData(filteredLocations.filter((item) => item !== null));
+        setLocationFetched(true);
       } catch (err) {
-        setError("Error fetching location data.");
+        setError(
+          "Error fetching location data. Please make sure you have given permission for location access."
+        );
         console.error(err);
       }
     };
 
-    fetchLocationData();
+    if (navigator.geolocation) {
+      fetchLocationData();
+    } else {
+      setError("Geolocation is not supported by your browser.");
+    }
   }, []);
 
   return (
@@ -38,38 +51,49 @@ export const Contact = () => {
       <div className="container portfolio__container">
         {error && <p className="error">{error}</p>}
 
-        {filteredData.length > 0 ? (
-          filteredData.map(({ id, name, description, image, location, source }, i) => (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={`/assets/${image}`} alt={name} key={i} id="card__image" />
-              </div>
-              <h3>{name}</h3>
-              <h5>{description}</h5>
-              <div className="portfolio__item-buttons">
-                <a
-                  href={location}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Location
-                </a>
+        {filteredData.length > 0
+          ? filteredData.map(
+              ({ id, name, description, image, location, source }, i) => (
+                <article key={id} className="portfolio__item">
+                  <div className="portfolio__item-image">
+                    <img
+                      src={`/assets/${image}`}
+                      alt={name}
+                      key={i}
+                      id="card__image"
+                    />
+                  </div>
+                  <h3>{name}</h3>
+                  <h5>{description}</h5>
+                  <div className="portfolio__item-buttons">
+                    <a
+                      href={location}
+                      className="btn btn-primary"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Location
+                    </a>
 
-                <a
-                  href={source}
-                  className="btn"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Contact
-                </a>
-              </div>
-            </article>
-          ))
-        ) : (
-          <p>No locations found within your radius.</p>
-        )}
+                    <a
+                      href={source}
+                      className="btn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Contact
+                    </a>
+                  </div>
+                </article>
+              )
+            )
+          : !error && (
+              <p>
+                {locationFetched
+                  ? "No locations found within your radius."
+                  : "Fetching Location please wait...."}
+              </p>
+            )}
       </div>
     </section>
   );
