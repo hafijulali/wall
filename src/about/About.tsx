bbef3a7c71343ee932a84ffd6ea16efb86c91b2a
Sheikh Hafijul Ali <hafijul.dev@gmail.com>
2025-02-14 22:51:31 +0530

    ADD : Dropdown for users to select radial distance

---
+++ src/about/About.tsx
@@ -0,0 +1,13 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <div id="about">
+      <h1>Ramadan Shehri</h1>
+      <div className="container about__container">
+      </div>
+    </div>
+  );
+};
+
+export default About;
